7bfc3e0b35ba4ad3113a0468eb041bc5b5306c41
Jesus Jose Romeo Claus <jesusromeoclaus123@gmail.com>
2026-06-18 17:55:26 -0300

    ImplementacionMecanicaBasica

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Phaser from "phaser";
+import Level1Scene from "./scenes/Level1Scene";
 
 const config = {
     type: Phaser.AUTO,
@@ -6,46 +7,7 @@ const config = {
     height: 600,
     backgroundColor: "#222222",
 
-    scene: {
-        create() {
-
-            this.player = this.add.rectangle(
-                400,
-                300,
-                50,
-                50,
-                0x00aaff
-            );
-
-            this.keys = this.input.keyboard.addKeys({
-                up: "W",
-                down: "S",
-                left: "A",
-                right: "D"
-            });
-        },
-
-        update() {
-
-            const speed = 4;
-
-            if (this.keys.left.isDown) {
-                this.player.x -= speed;
-            }
-
-            if (this.keys.right.isDown) {
-                this.player.x += speed;
-            }
-
-            if (this.keys.up.isDown) {
-                this.player.y -= speed;
-            }
-
-            if (this.keys.down.isDown) {
-                this.player.y += speed;
-            }
-        }
-    }
+    scene: [Level1Scene]
 };
 
 new Phaser.Game(config);
--- src/scenes/Level1Scene.js
+++ src/scenes/Level1Scene.js
@@ -1,51 +1,75 @@
 import Phaser from "phaser";
 
-const config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    backgroundColor: "#222222",
+export default class Level1Scene extends Phaser.Scene {
 
-    scene: {
-        create() {
+    constructor() {
+        super("Level1Scene");
+    }
 
-            this.player = this.add.rectangle(
-                400,
-                300,
-                50,
-                50,
-                0x00aaff
-            );
+    create() {
 
-            this.keys = this.input.keyboard.addKeys({
-                up: "W",
-                down: "S",
-                left: "A",
-                right: "D"
-            });
-        },
+        this.player = this.add.rectangle(
+            400,
+            300,
+            50,
+            50,
+            0x00aaff
+        );
+        this.aura = this.add.circle(
+        this.player.x,
+        this.player.y,
+            80,
+            0x00aaff,
+            0.25
+        );
+        this.colorObject = this.add.rectangle(
+            650,
+            300,
+            50,
+            50,
+            0x666666
+        );
+        this.keys = this.input.keyboard.addKeys({
+            up: "W",
+            down: "S",
+            left: "A",
+            right: "D"
+        });
+    }
 
-        update() {
+    update() {
 
-            const speed = 4;
+        const speed = 4;
 
-            if (this.keys.left.isDown) {
-                this.player.x -= speed;
-            }
+        if (this.keys.left.isDown) {
+            this.player.x -= speed;
+        }
 
-            if (this.keys.right.isDown) {
-                this.player.x += speed;
-            }
+        if (this.keys.right.isDown) {
+            this.player.x += speed;
+        }
 
-            if (this.keys.up.isDown) {
-                this.player.y -= speed;
-            }
+        if (this.keys.up.isDown) {
+            this.player.y -= speed;
+        }
 
-            if (this.keys.down.isDown) {
-                this.player.y += speed;
-            }
+        if (this.keys.down.isDown) {
+            this.player.y += speed;
+        }
+        this.aura.x = this.player.x;
+        this.aura.y = this.player.y;
+        const distance = Phaser.Math.Distance.Between(
+        this.player.x,
+        this.player.y,
+        this.colorObject.x,
+        this.colorObject.y
+        );
+
+        if (distance < 90) {
+            this.colorObject.fillColor = 0x00aaff;
+        }
+        else {
+            this.colorObject.fillColor = 0x666666;
         }
     }
-};
-
-new Phaser.Game(config);
+}
