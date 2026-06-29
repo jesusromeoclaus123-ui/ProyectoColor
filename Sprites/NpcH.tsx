1216db3f8254664d49ba205e5073f960bc0679ed
Jesus Jose Romeo Claus <jesusromeoclaus123@gmail.com>
2026-06-28 23:30:42 -0300

    Updates

---
+++ Sprites/NpcH.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="NpcH" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <image source="NpcH.png" width="120" height="120"/>
+</tileset>
---
+++ Sprites/Prota.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="Prota" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <image source="Prota.png" width="120" height="120"/>
+</tileset>
